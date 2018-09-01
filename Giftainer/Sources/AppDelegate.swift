e6a0fa7f922f1c56ba64c14556cdce9f6c7c3bba
Adrian Śliwa <adrian@adrenalin.io>
2018-09-01 15:29:10 +0200

    Initial commit

---
+++ Giftainer/Sources/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Giftainer
+//
+//  Created by Adrian Śliwa on 01/09/2018.
+//  Copyright © 2018 MobileSolutions. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {        
+        return true
+    }
+}
+
